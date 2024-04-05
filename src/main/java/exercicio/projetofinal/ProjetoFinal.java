7d8cbc192adc0adca40e29027d3002055fb0276b
Vitor Antonio Zidiotti de Souza <vitorzidiotti05@gmail.com>
2024-04-04 21:23:36 -0300

    initial comit :aerial_tramway:

---
+++ src/main/java/exercicio/projetofinal/ProjetoFinal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercicio.projetofinal;
+
+/**
+ *
+ * @author vitor
+ */
+public class ProjetoFinal {
+
+    public static void main(String[] args) {
+       HomePage form = new HomePage();
+       form.setVisible(true);
+    }
+}
